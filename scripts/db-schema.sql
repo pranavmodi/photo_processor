1899de6d0b3cc94aee7fff2050562bf6324182e5
Pranav <pranav.modi@gmail.com>
2019-05-22 17:37:13 +0200

    Task1: Update Schema

--- scripts/db-schema.sql
+++ scripts/db-schema.sql
@@ -3,11 +3,20 @@ CREATE EXTENSION IF NOT EXISTS pgcrypto;
 DROP TABLE IF EXISTS photos;
 DROP TYPE IF EXISTS photo_status;
 
-CREATE TYPE photo_status as enum('pending', 'completed');
+CREATE TYPE photo_status as enum('pending', 'completed', 'processing', 'failed');
 CREATE TABLE photos (
     uuid uuid DEFAULT gen_random_uuid() PRIMARY KEY,
     url text NOT NULL,
     status photo_status DEFAULT 'pending' NOT NULL,
+    created_at timestamp with time zone DEFAULT now() NOT NULL
+);
+
+CREATE TABLE photo_thumbnails (
+    uuid uuid DEFAULT gen_random_uuid() PRIMARY KEY,
+    photo_uuid uuid REFERENCES photos(uuid) NOT NULL,
+    width smallint NOT NULL,
+    height smallint NOT NULL,
+    url text NOT NULL,
     created_at timestamp with time zone DEFAULT now() NOT NULL
 );
 
