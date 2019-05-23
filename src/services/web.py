1f83033b9053ad9a9aa3051e94b24f8ae670a6ba
Pranav <pranav.modi@gmail.com>
2019-05-23 20:03:53 +0200

    Task 3: Add messages to Rabbit queue

--- src/services/web.py
+++ src/services/web.py
@@ -1,17 +1,37 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, g
 from flask_sqlalchemy import SQLAlchemy
+import pika
 
 database_uri = os.environ['PG_CONNECTION_URI']
+rabbitmq_uri = os.environ['AMQP_URI']
 
 app = Flask(__name__)
 
 app.config.update(
     SQLALCHEMY_DATABASE_URI=database_uri,
     SQLALCHEMY_TRACK_MODIFICATIONS=False,
+    RABBITMQ_URI=rabbitmq_uri
 )
 
 db = SQLAlchemy(app)
+
+def connect_queue():
+    if not hasattr(g, 'rabbitmq'):
+        conn_params = pika.URLParameters(app.config['RABBITMQ_URI'])
+        g.rabbitmq = pika.BlockingConnection(conn_params)
+    return g.rabbitmq
+
+
+def get_photo_processor_queue():
+    if not hasattr(g, 'photo_processor'):
+        conn = connect_queue()
+        channel = conn.channel()
+        channel.queue_declare(queue='photo_processor', durable=True)
+        channel.queue_bind(exchange='amq.direct', queue='photo_processor')
+        g.photo_processor = channel
+    return g.photo_processor
+
 
 @app.route("/")
 def index():
@@ -26,9 +46,16 @@ def get_pending_photos():
 
 @app.route("/photos/process", methods=["POST"])
 def process_photos():
-    from model import Photo
-    photos = Photo.query.all()
-    return jsonify(str(photos))
+    rdata = request.get_json()
+    photo_processor = get_photo_processor_queue()
+    q = get_photo_processor_queue()
+    for uuid in rdata['uuid']:
+        q.basic_publish(
+            exchange='amq.direct',
+            routing_key='photo_processor',
+            body=uuid)
+
+    return jsonify(success=True)
 
 
 if __name__ == '__main__':
